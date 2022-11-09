c162c0b51072d1f081d664ff7aab66602f612596
Marcelo Felix Salgado <marcelo.felixs@gmail.com>
2022-11-09 14:45:02 -0300

    Removing MySQL dependency

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,11 @@ FROM golang:1.16-alpine
 WORKDIR /app
 
 COPY go.mod ./
+COPY go.sum ./
 RUN go mod download
 
 COPY *.go ./
+COPY pkg/* ./domain/
 COPY web/* ./web/
 
 RUN go build -o /financial-balance-web
--- dump/dump.sql
+++
@@ -1,5 +0,0 @@
-create table users(id int not null auto_increment, name varchar(255), password varchar(255), primary key(id));
-
-create user 'financial_client'@'localhost' identified by 'financial_client';
-
-grant all privileges on financial_balance_db.* to 'financial_client'@'localhost';
